af55cb85e6fcfaf356f4e77e0e335624f6bfef12
NickShilin <127987796+NickShilin@users.noreply.github.com>
2023-03-28 13:26:24 +0300

    Figure

---
+++ src/main/java/ru/itsjava/hw2/Figure.java
@@ -0,0 +1,15 @@
+package ru.itsjava.hw2;
+
+public class Figure {
+
+
+    public void printSquare() {
+
+        System.out.println("Площадь Фигуры");
+    }
+
+    public void printPerimeter() {
+
+        System.out.println("Периметр Фигуры");
+    }
+}
